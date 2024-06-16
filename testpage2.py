bcde03b8011e4a7b0445f44432ba225b34694c07
Your Name <kwansawan50@gmail.com>
2024-06-16 15:56:29 +0700

    fix 4.5 ใกล้ได้

--- testpage2.py
+++ testpage2.py
@@ -3,30 +3,32 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/frompredict-page1', methods=['GET', 'POST'])
+@app.route('/frompredict-page1', methods=['POST'])
 def predict_box():
-    global predict_input_page1
+    global model_select_input, predict_input_page1, node0_input, node1_input
+    model_select_input = request.form.get('model_select')
     predict_input_page1 = request.form.get('frompredict')
-
-    return render_template('testpage.html', predict_input=predict_input_page1)
+    node0_input = request.form.get('node0input')
+    node1_input = request.form.get('node1input')
+    return render_template('testpage.html', predict_input=predict_input_page1, node0_input=node0_input, node1_input=node1_input)
 
 @app.route('/frompredict', methods=['POST'])
 def predict():
-    global predict_input, node0_input, node1_input
-    predict_input = predict_input_page1
+    global predict_input_page1, node0_input, node1_input
+    predict_input = request.form.get('frompredict')
     node0_input = request.form.get('node0input')
     node1_input = request.form.get('node1input')
-
     return redirect(url_for('evaluationpage', predict=predict_input, node0=node0_input, node1=node1_input))
 
 @app.route('/evaluationpage')
 def evaluationpage():
-    global predict_input, node0_input, node1_input
+    predict_input = request.args.get('predict')
+    node0_input = request.args.get('node0')
+    node1_input = request.args.get('node1')
     return render_template('evaluationpage.html', predict=predict_input, node0=node0_input, node1=node1_input)
 
 if __name__ == '__main__':
