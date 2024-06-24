f41517dfa064325b0e16d1f87a3eb430e165a6a9
Your Name <kwansawan50@gmail.com>
2024-06-24 09:24:31 +0700

    fix UI

--- testpage2.py
+++ testpage2.py
@@ -5,7 +5,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('testpage.html')
 
 @app.route('/frompredict-page1', methods=['POST'])
 def predict_box():
@@ -27,14 +27,14 @@ def predict():
     predict_input = request.form.get('frompredict')
     node0_input = request.form.get('node0input')
     node1_input = request.form.get('node1input')
-    return redirect(url_for('evaluationpage', predict=predict_input, node0=node0_input, node1=node1_input))
+    return redirect(url_for('shappercentile', predict=predict_input, node0=node0_input, node1=node1_input))
 
 @app.route('/shappercentile')
 def evaluationpage():
     predict_input = request.args.get('predict')
     node0_input = request.args.get('node0')
     node1_input = request.args.get('node1')
-    return render_template('evaluationpage.html', predict=predict_input, node0=node0_input, node1=node1_input)
+    return render_template('shappercentile.html', predict=predict_input, node0=node0_input, node1=node1_input)
 
 if __name__ == '__main__':
     app.run(debug=True)
