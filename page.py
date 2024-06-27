6d8e5912aca6e65799afd593bbd5d2bc003cbdea
Your Name <kwansawan50@gmail.com>
2024-06-27 14:55:47 +0700

    fix func 4.5

--- page.py
+++ page.py
@@ -10,12 +10,11 @@ def index():
 @app.route('/frompredict-page1', methods=['POST'])
 def shappage():
     global model_select_input, predict_input_page1, node0_input, node1_input
-
     model_select_input = request.form.get('model_select')
-    predict_input_page1 = request.form.get('frompredict')
-    node0_input = request.form.get('node0input')
-    node1_input = request.form.get('node1input')
-
+    predict_input_page1 = request.form.get('predict_input')
+    node0_input = request.form.get('node0_input')
+    node1_input = request.form.get('node0_input')
+    
     print(f'model_select_input: {model_select_input}')
     print(f'predict_input_page1: {predict_input_page1}')
     print(f'node0_input: {node0_input}')
@@ -25,30 +24,28 @@ def shappage():
 
 @app.route('/shappercentile', methods=['POST'])
 def shappercentile():
-
-    predict_input_page = request.form.get('predict_input')
-    node0_input_page = request.form.get('node0_input')
-    node1_input_page = request.form.get('node1_input')
-
-    print('a')
-    print(f'predict_input_page: {predict_input_page}')
-    print(f'node0_input: {node0_input_page}')
-    print(f'node1_input: {node1_input_page}')
+    global predict_input_page1, node0_input, node1_input
+    predict_input_page = request.form.get('frompredict')
+    node0_input_page = request.form.get('node0input')
+    node1_input_page = request.form.get('node1input')
 
     return render_template('shappercentile.html', predict_input1=predict_input_page, node0=node0_input_page, node1=node1_input_page)
+
+@app.route('/shappercentilepage')
+def shap():
+    predict_input = request.args.get('predict_input1')
+    node0_input = request.args.get('node0')
+    node1_input = request.args.get('node1')
+    return render_template('shappercentile.html', predict_input1=predict_input, node0=node0_input, node1=node1_input)
 
 
 @app.route('/evaluationpage', methods=['POST'])
 def evaluationpage():
-    predict_input_page = request.form.get('predict_input')
-    node0_input_page = request.form.get('node0_input')
-    node1_input_page = request.form.get('node1_input')
+    predict_input4 = request.form.get('predict_input')
+    node0_input4 = request.form.get('node0_input')
+    node1_input4 = request.form.get('node1_input')
 
-    print(f'predict_input_page4: {predict_input_page}')
-    print(f'node0_input4: {node0_input_page}')
-    print(f'node1_inpu4t: {node1_input_page}')
-    return render_template('evaluationpage.html', predict1=predict_input_page, node0_1=node0_input_page, node1_1=node1_input_page)
-
+    return redirect(url_for('evaluationpage', predict=predict_input4, node0=node0_input4, node1=node1_input4))
 
 if __name__ == '__main__':
     app.run(debug=True)
